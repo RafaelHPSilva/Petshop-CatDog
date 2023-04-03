efb3a0e91e2708c7e6180a9bd126cbdf6400db5f
Rafael Silva <rafael_rhps@outlook.com>
2023-04-03 19:20:27 -0300

    CSS do cabecalho concluído

---
+++ src/components/cabecalho.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import imagem from "../assets/img/doguito.svg";
+import '../assets/css/componentes/cabecalho.css'
+import { Link } from 'react-router-dom'
+
+const Cabecalho = ()=>{
+
+  return(
+    <header className='cabecalho container'>
+      <div className='menu-hamburger'>
+        <span className='menu-hamburger__icone'></span>
+      </div>
+
+      <div className='cabecalho-container'>
+        <Link to='/' className='flex flex--centro'>
+          <img className='cabecalho__logo' src={imagem} alt="logo doguito"></img>
+          <h1 className='cabecalho__titulo'>PetShop</h1>
+        </Link>
+      </div>
+
+      <nav className='menu-cabecalho' >
+        <ul className='menu-itens'>
+          <li><Link to='#' className='menu-item menu-item--entrar'>Entrar</Link></li>
+          <li><Link to='/Sobre' className='menu-item'>Sobre</Link></li>
+          <li><Link to='#' className='menu-item'>Produtos</Link></li>
+          <li><Link to='/Blog' className='menu-item'>Blog</Link></li>
+        </ul>
+      </nav>
+      <div className='menu-cabecalho-background'></div>
+    </header>
+  )
+}
+
+export default Cabecalho
